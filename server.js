e3d760dfcf31189e19a3637f404994b764df584b
Cheol Park <goodykimchi@gmail.com>
2026-02-05 20:19:35 +0900

    Add DELETE method to CORS

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ const app = express();
 // ========== CORS 설정 추가 ==========
 app.use(cors({
     origin: '*',
-    methods: ['GET', 'POST', 'OPTIONS'],
+    methods: ['GET', 'POST', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
 }));
