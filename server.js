33454aa99bbe2da1cb443ebc697410380647f617
keytrend <m70park@gmail.com>
2026-02-13 09:29:55 +0900

    Update server.js

--- server.js
+++ server.js
@@ -33,7 +33,6 @@ app.use(cors({
     origin: '*',
     methods: ['GET', 'POST', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true
 }));
 // ===================================
 
